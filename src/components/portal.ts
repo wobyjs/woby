2db2225c1deb5edc471b025ac144b6963b25cdd6
wongchichong <wongchichong@gmail.com>
2024-02-06 10:23:26 +0800

    bump version

--- src/components/portal.ts
+++ src/components/portal.ts
@@ -1,54 +1,54 @@
 
 /* IMPORT */
 
-import useBoolean from '../hooks/use_boolean';
-import useRenderEffect from '../hooks/use_render_effect';
-import render from '../methods/render';
-import $$ from '../methods/SS';
-import { createHTMLNode } from '../utils/creators';
-import { assign } from '../utils/lang';
-import type { Child, ChildWithMetadata, FunctionMaybe } from '../types';
+import useBoolean from '../hooks/use_boolean'
+import useRenderEffect from '../hooks/use_render_effect'
+import render from '../methods/render'
+import $$ from '../methods/SS'
+import { createHTMLNode } from '../utils/creators'
+import { assign } from '../utils/lang'
+import type { Child, ChildWithMetadata, FunctionMaybe } from '../types'
 
 /* MAIN */
 
-const Portal = ( { when = true, mount, wrapper, children }: { mount?: Child, when?: FunctionMaybe<boolean>, wrapper?: Child, children: Child } ): ChildWithMetadata<{ portal: HTMLElement }> => {
+const Portal = ({ when = true, mount, wrapper, children }: { mount?: Child, when?: FunctionMaybe<boolean>, wrapper?: Child, children?: Child }): ChildWithMetadata<{ portal: HTMLElement }> => {
 
-  const portal = $$( wrapper ) || createHTMLNode( 'div' );
+    const portal = $$(wrapper) || createHTMLNode('div')
 
-  if ( !( portal instanceof HTMLElement ) ) throw new Error( 'Invalid wrapper node' );
+    if (!(portal instanceof HTMLElement)) throw new Error('Invalid wrapper node')
 
-  const condition = useBoolean( when );
+    const condition = useBoolean(when)
 
-  useRenderEffect( () => {
+    useRenderEffect(() => {
 
-    if ( !$$( condition ) ) return;
+        if (!$$(condition)) return
 
-    const parent = $$( mount ) || document.body;
+        const parent = $$(mount) || document.body
 
-    if ( !( parent instanceof Element ) ) throw new Error( 'Invalid mount node' );
+        if (!(parent instanceof Element)) throw new Error('Invalid mount node')
 
-    parent.insertBefore( portal, null );
+        parent.insertBefore(portal, null)
 
-    return (): void => {
+        return (): void => {
 
-      parent.removeChild( portal );
+            parent.removeChild(portal)
 
-    };
+        }
 
-  } );
+    })
 
-  useRenderEffect( () => {
+    useRenderEffect(() => {
 
-    if ( !$$( condition ) ) return;
+        if (!$$(condition)) return
 
-    return render( children, portal );
+        return render(children, portal)
 
-  } );
+    })
 
-  return assign( () => $$( condition ) || children, { metadata: { portal } } );
+    return assign(() => $$(condition) || children, { metadata: { portal } })
 
-};
+}
 
 /* EXPORT */
 
-export default Portal;
+export default Portal
